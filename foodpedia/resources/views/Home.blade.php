8c965f51f02056f147ae7209926365e618b62464
Mario Manurung <marioandreas263@gmail.com>
2023-05-09 20:39:59 +0700

    +++

---
+++ foodpedia/resources/views/Home.blade.php
@@ -0,0 +1,177 @@
+@include('struktur.navbar')
+
+    <!-- Blog Start -->
+    <div class="container-fluid pt-5">
+      <div class="container">
+        <div class="text-center pb-2">
+          <p class="section-title px-5">
+            <span class="px-2">Menu</span>
+          </p>
+          <h1 class="mb-4">apa carik kak?</h1>
+        </div>
+        <div class="row pb-3">
+          <div class="col-lg-4 mb-4">
+            <div class="card border-0 shadow-sm mb-2">
+              <img class="card-img-top mb-2" src="img/blog-1.jpg" alt="" />
+              <div class="card-body bg-light text-center p-4">
+                <h4 class="">Makanan</h4>
+                <p>
+                  Sed kasd sea sed at elitr sed ipsum justo, sit nonumy diam
+                  eirmod, duo et sed sit eirmod kasd clita tempor dolor stet
+                  lorem. Tempor ipsum justo amet stet...
+                </p>
+                <a href="" class="btn btn-primary px-4 mx-auto my-2"
+                  >Read More</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4">
+            <div class="card border-0 shadow-sm mb-2">
+              <img class="card-img-top mb-2" src="img/blog-2.jpg" alt="" />
+              <div class="card-body bg-light text-center p-4">
+                <h4 class="">Minuman</h4>
+                <p>
+                  Sed kasd sea sed at elitr sed ipsum justo, sit nonumy diam
+                  eirmod, duo et sed sit eirmod kasd clita tempor dolor stet
+                  lorem. Tempor ipsum justo amet stet...
+                </p>
+                <a href="" class="btn btn-primary px-4 mx-auto my-2"
+                  >Read More</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4">
+            <div class="card border-0 shadow-sm mb-2">
+              <img class="card-img-top mb-2" src="img/blog-3.jpg" alt="" />
+              <div class="card-body bg-light text-center p-4">
+                <h4 class="">Snack</h4>
+                <p>
+                  Sed kasd sea sed at elitr sed ipsum justo, sit nonumy diam
+                  eirmod, duo et sed sit eirmod kasd clita tempor dolor stet
+                  lorem. Tempor ipsum justo amet stet...
+                </p>
+                <a href="" class="btn btn-primary px-4 mx-auto my-2"
+                  >Read More</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Blog End -->
+
+    <!-- Footer Start -->
+    <div
+      class="container-fluid bg-secondary text-white mt-5 py-5 px-sm-3 px-md-5"
+    >
+      <div class="row pt-5">
+        <div class="col-lg-3 col-md-6 mb-5">
+          <a
+            href=""
+            class="navbar-brand font-weight-bold text-primary m-0 mb-4 p-0"
+            style="font-size: 40px; line-height: 40px"
+          >
+          <h3 class="text-primary mb-4">foodpedia</h3>
+          </a>
+          <p>
+            Ngantuk? Ya tidur
+            <br>Suka? Ya tembak
+            <br>Haus? Ya minum
+            <br>Laper? Ya makan
+            <br>Gabut? Ya nongkrong di foodpedia
+          </p>
+          <div class="d-flex justify-content-start mt-4">
+            <a
+              class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
+              style="width: 38px; height: 38px"
+              href="#"
+              ><i class="fab fa-facebook-f"></i
+            ></a>
+            <a
+              class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
+              style="width: 38px; height: 38px"
+              href="#"
+              ><i class="fab fa-whatsapp"></i
+            ></a>
+            <a
+              class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
+              style="width: 38px; height: 38px"
+              href="#"
+              ><i class="fab fa-instagram"></i
+            ></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5">
+          <h3 class="text-primary mb-4">Get In Touch</h3>
+          <div class="d-flex">
+            <h4 class="fa fa-map-marker-alt text-primary"></h4>
+            <div class="pl-3">
+              <h5 class="text-white">Address</h5>
+              <p>Jl. Dr. TB. Silalahi, Silalahi Pagar Batu, Kec. Balige, Toba, Sumatera Utara 22312</p>
+            </div>
+          </div>
+          <div class="d-flex">
+            <h4 class="fa fa-envelope text-primary"></h4>
+            <div class="pl-3">
+              <h5 class="text-white">Email</h5>
+              <p>foodpedia@gmail.com            </p>
+            </div>
+          </div>
+          <div class="d-flex">
+            <h4 class="fa fa-phone-alt text-primary"></h4>
+            <div class="pl-3">
+              <h5 class="text-white">Phone</h5>
+              <p>+012 345 67890</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5">
+          <h3 class="text-primary mb-4">Opening</h3>
+          <div class="d-flex flex-column justify-content-start">
+            <a class="text-white mb-2"
+              ><i class="fa fa-angle-right mr-2"></i>Senin-Jumat : 11.00 - 21.00</a
+            >
+            <a class="text-white mb-2"
+              ><i class="fa fa-angle-right mr-2"></i>Sabtu - Minggu : 11.00 - 22.00</a
+            >
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5">
+      </div>
+      <div
+        class="container-fluid pt-5"
+        style="border-top: 1px solid rgba(23, 162, 184, 0.2) ;"
+      >
+        <p class="m-0 text-center text-white">
+          &copy;
+          <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+          Designed by Kelompok 06 PA 2023
+        </p>
+      </div>
+    </div>
+    <!-- Footer End -->
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary p-3 back-to-top"
+      ><i class="fa fa-angle-double-up"></i
+    ></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('/FrontEnd/lib/easing/easing.min.js')}}"></script>
+    <script src="{{asset('/FrontEnd/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('/FrontEnd/lib/isotope/isotope.pkgd.min.js')}}"></script>
+    <script src="{{asset('/FrontEnd/lib/lightbox/js/lightbox.min.js')}}"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="{{asset('/FrontEnd/mail/jqBootstrapValidation.min.js')}}"></script>
+    <script src="{{asset('/FrontEnd/mail/contact.js')}}"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{asset('/FrontEnd/js/main.js')}}"></script>
+  </body>
+</html>
